afa1b6a3347627fe7d60d007efa8ab21b738e73c
yesica <doradoyesica@gmail.com>
2024-05-28 20:39:45 -0300

    acerca de

--- assets/js/miarchivo.js
+++ assets/js/miarchivo.js
@@ -21,3 +21,9 @@ const navMenu = document.getElementById('nav-menu'),
         navMenu.classList.remove('show-menu')
     }
     navLink.forEach(n => n.addEventListener('click', linkAction))
+
+    function scrollHeader(){
+        const header = document.getElementById('header')
+        if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    }
+window.addEventListener('scroll', scrollHeader)
